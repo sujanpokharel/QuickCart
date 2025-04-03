221fc57927510f58e4be98398d18d5518df80268
sujanpokharel <pokharelsujan07@gmail.com>
2025-04-03 11:17:11 +0545

    Move API route from api/inngest to app/inngest

--- app/inngest/route.js
+++ app/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/config/inngest";
-import { syncUserCreation, syncUserDeletion } from "../../config/inngest";
+import { syncUserCreation, syncUserDeletion } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
